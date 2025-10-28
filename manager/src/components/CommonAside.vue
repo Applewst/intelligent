69e24915c1ca48a1f47b067ea2ede83284c27d8d
Monke <2685986106@qq.com>
2025-10-28 11:39:25 +0800

    侧边栏完成

--- manager/src/components/CommonAside.vue
+++ manager/src/components/CommonAside.vue
@@ -26,6 +26,7 @@
       path:'/other',
       name:'other',
       label:'其他',
+      icon:'location',
       children:[
         {
           path:'page1',
@@ -44,51 +45,74 @@
       ]
     }
   ])
-  const noChild = computed(() => { list.value.filter(item => !item.children) })
-  const hasChild = computed(() => { list.value.filter(item =>  item.children) })  //看是否需要二级目录
+  const noChild = computed(() => list.value.filter(item => !item.children) )
+  const hasChild = computed(() => list.value.filter(item => item.children) )  //看是否需要二级目录
+  console.log(hasChild.value);
+  
 </script>
 
 <template>
   <el-aside width="180px">
     <el-menu
-  
-     
-    >
-    <h3>后台管理系统</h3>
-      <el-menu-item
-        v-for="item in noChild"
-        :index="item.path"
-        :key="item.path"
-        >
-        <component class="icons" :is="item.icon"></component>
-        <span>{{item.label}}</span>
-     </el-menu-item>
-      <!-- 二级目录 -->
-      <el-sub-menu
-        v-for="item in hasChild"
-        :index="item.path"
-        :key="item.path"
+      background-color="#545c64"
+      text-color="#fff"
+      
       >
-          <template #title>
-            <component class="icons" :is="item.icon"></component>
-            <span>{{item.label}}</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item
-              v-for="(subItem,subIndex) in hasChild"
-              :index="subItem.path"
-              :key="subItem.path"
-            >
-              <component class="icons" :is="subItem.icon"></component>
-              <span>{{subItem.label}}</span>
+      <h3>后台管理系统</h3>
+      <el-menu-item 
+        v-for="item in noChild"
+        :key="item.path"
+        :index="item.path"
+        
+      >
+      
+          <component :is="item.icon" class="icons"></component>
+          <span>{{item.label}}</span>
+      </el-menu-item>
+      <el-sub-menu 
+        v-for="item in hasChild"
+        :key="item.path"
+        :index="item.path"
+      >
+        <template #title>
+          <component :is="item.icon" class="icons"></component>
+          <span>{{item.label}}</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item 
+            v-for="(subItem,subIndex) in item.children"
+            :key="subIndex.path"
+            :index="subIndex.path"
+          >
+          <component :is="subItem.icon" class="icons"></component>
+            {{subItem.label}}
           </el-menu-item>
-          </el-menu-item-group>
+        </el-menu-item-group>
+       
+     
+        </el-sub-menu>
 
-      </el-sub-menu>
-    </el-menu>
+        
+      </el-menu>
   </el-aside>
 </template>
 
 <style scoped lang="less">
-
+.icons{
+  width: 18px;
+  height: 18px;
+  margin-right: 5px;
+}
+.el-menu{
+  border-right:none;
+  h3{
+    line-height:48px;
+    color:#fff;
+    text-align: center;
+  }
+}
+.el-aside{
+  height: 100%;
+  background-color: #545c64;
+}
 </style>
