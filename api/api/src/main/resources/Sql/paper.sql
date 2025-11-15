568e3aadfb708381753719727de482fb3f3777d5
Askr-Yggdrasill <asd19870510555@163.com>
2025-11-15 17:26:49 +0800

    paper表添加data

--- api/api/src/main/resources/Sql/paper.sql
+++ api/api/src/main/resources/Sql/paper.sql
@@ -4,6 +4,7 @@ CREATE TABLE `paper`
     `title`  varchar(35) NOT NULL COMMENT '标题',
     `author` varchar(30) DEFAULT NULL COMMENT '作者',
     `file`   varchar(60) DEFAULT NULL COMMENT '文件URL',
+    'time'   date comment '发表时间',
     `detail` text COMMENT '描述',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
