35247fe10f2702e0335328031e9c54f2d90c0c8f
kkstar <give4178@163.com>
2025-11-19 23:29:14 +0800

    更新init.sql.

--- api/api/src/main/resources/Sql/init.sql
+++ api/api/src/main/resources/Sql/init.sql
@@ -271,15 +271,14 @@ VALUES ('祁婷婷', '神经网络', 'ob5j6i_gl4ivm@qq.com', 'https://avatars.gi
 -- 团队介绍
 CREATE TABLE `team`
 (
-    `id`           INT         NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `team_name`    VARCHAR(20) NOT NULL COMMENT '团队名称',
-    `introduction` TEXT COMMENT '团队介绍',
-    `image_url`    VARCHAR(50) DEFAULT NULL COMMENT '图片',
+    `id`           int         NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `team_name`    varchar(20) NOT NULL COMMENT '团队名称',
+    `introduction` text COMMENT '团队介绍',
+    `image_url`    varchar(256) DEFAULT NULL COMMENT '图片',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci
-    COMMENT ='团队介绍';
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='团队介绍';
 
 -- 示例数据
 INSERT INTO `team` (`team_name`, `introduction`, `image_url`)
